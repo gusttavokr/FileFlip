61886f4d0cfa42f08dd772f06af3d5947813bb3d
Gustavo Maia <gustavommilitaoo@gmail.com>
2025-10-21 11:24:45 +0000

    :lipstick: feat: tela de registro

--- frontend/fileflip/src/app/app.ts
+++ frontend/fileflip/src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Registro } from './pages/registro/registro';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Registro
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
