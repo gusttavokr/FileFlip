2a6c8d4b73f0283729a53c67691e981f0e388708
Gustavo Maia <gustavommilitaoo@gmail.com>
2025-10-22 12:02:23 +0000

    :construction: wip: tela de registro

--- frontend/fileflip/src/app/pages/registro/registro.ts
+++ frontend/fileflip/src/app/pages/registro/registro.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectModule } from 'primeng/select';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { PasswordModule } from 'primeng/password';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-  selector: 'app-registro',
-  imports: [],
-  templateUrl: './registro.html',
+    selector: 'app-registro',
+    templateUrl: './registro.html',
+    standalone: true,
+    imports: [
+        FormsModule, 
+        InputGroupModule, 
+        InputGroupAddonModule, 
+        InputTextModule, 
+        SelectModule, 
+        InputNumberModule,
+        PasswordModule,
+        ButtonModule
+    ]
 })
 export class Registro {
+    username: string | undefined;
 
+    email: string | undefined;
+
+    senha!: string;
+    confirmarSenha!: string;
 }
