ee29b689af5c97a6c76f72d77a1ac9b386d42078
Gustavo Maia <gustavommilitaoo@gmail.com>
2025-11-06 13:09:40 +0000

    :sparkles: feat: login + ajuste de rotas

--- frontend/fileflip/src/app/app.routes.ts
+++ frontend/fileflip/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { Registro } from './pages/registro/registro';
 import { Index } from './pages/index/index';
 import { Arquivos } from './pages/arquivos/arquivos';
+import { Login } from './pages/login/login';
 
 export const routes: Routes = [
-  { path: '', component: Index },        // rota raiz
-  { path: 'registro', component: Registro },        // rota registro
-  { path: 'meus-arquivos', component: Arquivos },  // rota /arquivos
+  { path: '', component: Index },        
+  { path: 'registro', component: Registro },        
+  { path: 'meus-arquivos', component: Arquivos },  
+  { path: 'login', component: Login },         
 ];
