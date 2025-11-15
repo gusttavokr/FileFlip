2b801ad0f4324ea96dba6069111ee1e4c3dcdb37
Gustavo Maia <gustavommilitaoo@gmail.com>
2025-11-15 01:14:14 +0000

    :sparkles: feat: entity user

--- backend/auth_service/src/main/java/com/fileflip/auth_service/User.java
+++ backend/auth_service/src/main/java/com/fileflip/auth_service/User.java
@@ -1,8 +1,19 @@
 package com.fileflip.auth_service;
 
-import jakarta.persistence.Entity;
+import java.util.Set;
+import java.util.UUID;
+
+import jakarta.persistence.*;
 
 @Entity
+@Table(name="tb.users")
 public class User{
+    @Id
+    @GeneratedValue(strategy=GenerationType.UUID)
+    private UUID userId;
 
+    private String username;
+    private String password;
+
+    // private Set<Role> roles;
 }
