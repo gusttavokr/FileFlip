5fcbcd18821f699cf7b7f02ee024cd9a6f901efe
Gustavo Maia <gustavommilitaoo@gmail.com>
2025-11-22 18:41:37 +0000

    :bug: fix: tamanho máximo de arquivos

--- frontend/fileflip/src/app/pages/index/index.ts
+++ frontend/fileflip/src/app/pages/index/index.ts
@@ -113,17 +113,17 @@ export class Index {
   }
 
   formatSize(bytes: number): string {
-      const k = 1024;
-      const dm = 3;
-      const sizes = this.fileSizeTypes;
-      if (!bytes || bytes === 0) {
-          return `0 ${sizes[0]}`;
-      }
+        const k = 1024;
+        const sizes = this.fileSizeTypes;
+        
+        if (!bytes || bytes === 0) {
+            return `0 ${sizes[0]}`;
+        }
 
-      const i = Math.floor(Math.log(bytes) / Math.log(k));
-      const formattedSize = parseFloat((bytes / Math.pow(k, i)).toFixed(dm));
+        const i = Math.floor(Math.log(bytes) / Math.log(k));
+        const formattedSize = parseFloat((bytes / Math.pow(k, i)).toFixed(2)); // 2 casas decimais
 
-      return `${formattedSize} ${sizes[i] ?? sizes[sizes.length - 1]}`;
-  }
+        return `${formattedSize} ${sizes[i] ?? sizes[sizes.length - 1]}`;
+    }
 }
 // ...existing code...
