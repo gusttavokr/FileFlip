81966583fe95d12d5d28c2e9a63d7809f26589c6
Gustavo Maia <gustavommilitaoo@gmail.com>
2025-11-24 10:54:45 +0000

    :recycle: refactor: gateway em drf

---
+++ backend/fileflip_gateway/gateway/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from .views import (
+    ListarArquivosView,
+    ConverterArquivoView,
+    LoginView,
+    CadastrarUsuarioView,
+    VincularGoogleView,
+    AtualizarUsuarioView,
+    ListarUsuariosView,
+    DeletarUsuarioView,
+    ListarArquivosSoapView
+)
+
+urlpatterns = [
+    # arquivo_service
+    path("arquivos", ListarArquivosView.as_view()),
+    path("arquivos/converter", ConverterArquivoView.as_view()),
+
+    # auth_service
+    path("auth/login", LoginView.as_view()),
+    path("auth/usuarios", CadastrarUsuarioView.as_view()),
+    path("auth/usuarios/listar", ListarUsuariosView.as_view()),
+    path("auth/usuarios/<uuid:id>", AtualizarUsuarioView.as_view()),
+    path("auth/usuarios/<uuid:id>/delete", DeletarUsuarioView.as_view()),
+    path("auth/usuarios/<uuid:id>/vincular-google", VincularGoogleView.as_view()),
+
+    # soap
+    path("soap/arquivos", ListarArquivosSoapView.as_view()),
+]
